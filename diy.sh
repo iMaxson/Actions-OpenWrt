dc950e2c0ed459105d46893138c3653054ef6b89
iMaxson <62035199+iMaxson@users.noreply.github.com>
2020-03-20 21:13:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,3 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/vernesong/OpenClash.git
